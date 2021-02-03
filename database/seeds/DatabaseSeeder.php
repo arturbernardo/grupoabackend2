f45de69a25719248404cb501ecc6e1e91ea932a8
arturbernardo <artur.bernardo.k@gmail.com>
2021-02-03 18:32:40 -0300

    default user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,28 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        if(User::where('email','=','admin@mail.com')->count()){
+            $user = User::where('email','=','admin@mail.com')->first();
+        }else{
+            $user = new User;
+        }
+
+        $user->name = "Admin";
+        $user->email = "admin@mail.com";
+        $user->password = bcrypt("123456");
+        $user->save();
+
+
     }
 }
